afb27d2ecc855fea5c02ce361998e29860a3c5c7
Mohammad <mamad.jafari91@gmail.com>
2023-02-11 14:33:57 +0330

    remove redundant transformation

--- PANDA/dataset.py
+++ PANDA/dataset.py
@@ -9,15 +9,13 @@ from glob import glob
 
 transform_color = transforms.Compose([transforms.Resize(256),
                                       transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+                                      transforms.ToTensor()])
 
 transform_gray = transforms.Compose([
                                  transforms.Resize(256),
                                  transforms.CenterCrop(224),
                                  transforms.Grayscale(num_output_channels=3),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                 transforms.ToTensor()
                                 ])
 
 mvtec_labels = ['bottle', 'cable', 'capsule', 'carpet', 'grid', 'hazelnut', 'leather',
