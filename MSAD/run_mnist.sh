494d212a40214b1ba6e331bb4ae975277ff13523
Mohammad <mamad.jafari91@gmail.com>
2023-02-28 21:44:35 +0330

    modify epochs and batch size

--- MSAD/run_mnist.sh
+++ MSAD/run_mnist.sh
@@ -7,14 +7,6 @@ sudo nvidia-smi -i 0
 for (( n=0; n<10; n++ ))
 do
   echo "Running MSAD on MNIST with label $n"
-  python main.py --dataset=mnist --label=$n --backbone=18 --dataset_path=$DATASET_PATH
-  sudo nvidia-smi --gpu-reset
-done
-
-
-for (( n=0; n<10; n++ ))
-do
-  echo "Running MSAD on MNIST with label $n - ResNet 152"
-  python main.py --dataset=mnist --label=$n --backbone=152 --dataset_path=$DATASET_PATH
+  python main.py --dataset=mnist --label=$n --backbone=18 --dataset_path=$DATASET_PATH --epochs=5
   sudo nvidia-smi --gpu-reset
 done
