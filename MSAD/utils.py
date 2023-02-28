a9624913930a48a2a8097637b7e23323b2c705fc
Mohammad <mamad.jafari91@gmail.com>
2023-02-28 12:29:40 +0330

    add dataset support MSAD

--- MSAD/utils.py
+++ MSAD/utils.py
@@ -9,6 +9,11 @@ from PIL import ImageFilter
 import random
 from torchvision.transforms import InterpolationMode
 BICUBIC = InterpolationMode.BICUBIC
+from torchvision.datasets import CIFAR10, MNIST, SVHN, FashionMNIST
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+from glob import glob
 
 class GaussianBlur(object):
     """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
@@ -32,6 +37,17 @@ transform_resnet18 = transforms.Compose([
     transforms.ToTensor()])
 
 
+transform_color_bw = transforms.Compose([transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.Grayscale(num_output_channels=3),
+                                      transforms.ToTensor()])
+
+transform_resnet18_bw = transforms.Compose([
+    transforms.Resize(224, interpolation=BICUBIC),
+    transforms.CenterCrop(224),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.ToTensor()])
+
 
 moco_transform = transforms.Compose([
     transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
@@ -43,6 +59,16 @@ moco_transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor()])
 
+moco_transform_bw = transforms.Compose([
+    transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
+    transforms.Grayscale(num_output_channels=3),
+    transforms.RandomApply([
+        transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
+    ], p=0.8),
+    transforms.RandomGrayscale(p=0.2),
+    transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor()])
 
 
 mean = [0.485, 0.456, 0.406]
@@ -52,17 +78,8 @@ mu = torch.tensor(mean).view(3,1,1).cuda()
 std = torch.tensor(std).view(3,1,1).cuda()
 
 class Transform:
-    def __init__(self):
-        self.moco_transform = transforms.Compose([
-            transforms.RandomResizedCrop(224, scale=(0.2, 1.)),
-            transforms.RandomApply([
-                transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)  # not strengthened
-            ], p=0.8),
-            transforms.RandomGrayscale(p=0.2),
-            transforms.RandomApply([GaussianBlur([.1, 2.])], p=0.5),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor()])
-
+    def __init__(self, bw=False):
+        self.moco_transform = moco_transform_bw if bw else moco_transform
     def __call__(self, x):
         x_1 = self.moco_transform(x)
         x_2 = self.moco_transform(x)
@@ -101,7 +118,6 @@ def freeze_parameters(model, backbone, train_fc=False):
             p.requires_grad = False
 
 
-
 def knn_score(train_set, test_set, n_neighbours=2):
     """
     Calculates the KNN distance
@@ -112,26 +128,153 @@ def knn_score(train_set, test_set, n_neighbours=2):
     return np.sum(D, axis=1)
 
 
-def get_loaders(dataset, label_class, batch_size, backbone):
-    if dataset == "cifar10":
-        ds = torchvision.datasets.CIFAR10
-        transform = transform_color if backbone == 152 else transform_resnet18
-        coarse = {}
-        trainset = ds(root='data', train=True, download=True, transform=transform, **coarse)
-        testset = ds(root='data', train=False, download=True, transform=transform, **coarse)
-        trainset_1 = ds(root='data', train=True, download=True, transform=Transform(), **coarse)
-        idx = np.array(trainset.targets) == label_class
-        testset.targets = [int(t != label_class) for t in testset.targets]
-        trainset.data = trainset.data[idx]
-        trainset.targets = [trainset.targets[i] for i, flag in enumerate(idx, 0) if flag]
-        trainset_1.data = trainset_1.data[idx]
-        trainset_1.targets = [trainset_1.targets[i] for i, flag in enumerate(idx, 0) if flag]
-        train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2,
-                                                   drop_last=False)
-        test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2,
-                                                  drop_last=False)
-        return train_loader, test_loader, torch.utils.data.DataLoader(trainset_1, batch_size=batch_size,
-                                                                      shuffle=True, num_workers=2, drop_last=False)
+def get_loaders(dataset, label_class, batch_size, path, backbone):
+
+    if dataset == 'cifar10':
+        return get_CIFAR10(label_class, batch_size, path, backbone)
+    elif dataset == 'mnist':
+        return get_MNIST(label_class, batch_size, path, backbone)
+    elif dataset == 'fashion':
+        return get_FASHION_MNIST(label_class, batch_size, path, backbone)
+    elif dataset == 'svhn':
+        return get_SVHN(label_class, batch_size, path, backbone)
+    elif dataset == 'mvtec':
+        return get_MVTEC(label_class, batch_size, path, backbone)
     else:
-        print('Unsupported Dataset')
+        raise Exception("Dataset is not supported yet. ")
         exit()
+        
+
+mvtec_labels = ['bottle', 'cable', 'capsule', 'carpet', 'grid', 'hazelnut', 'leather',
+                'metal_nut', 'pill', 'screw', 'tile', 'toothbrush', 'transistor',
+                'wood', 'zipper']
+
+
+def get_CIFAR10(normal_class_indx, batch_size, path, backbone):
+    transform = transform_color if backbone == 152 else transform_resnet18
+
+    trainset = CIFAR10(root=path, train=True, download=True, transform=transform)
+    trainset.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
+    trainset.targets  = [0 for t in trainset.targets]
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testset = CIFAR10(root=path, train=False, download=True, transform=transform)
+    testset.targets  = [int(t!=normal_class_indx) for t in testset.targets]
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    trainset_msad = CIFAR10(root=path, train=True, download=True, transform=Transform())
+    trainset_msad.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
+    trainset_msad.targets  = [0 for t in trainset.targets]
+    train_loader_msad = torch.utils.data.DataLoader(trainset_msad, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return train_loader, test_loader, train_loader_msad
+
+
+
+def get_MNIST(normal_class_indx, batch_size, path, backbone):
+    transform = transform_color_bw if backbone == 152 else transform_resnet18_bw
+
+    trainset = MNIST(root=path, train=True, download=True, transform=transform)
+    trainset.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
+    trainset.targets  = [0 for t in trainset.targets]
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testset = MNIST(root=path, train=False, download=True, transform=transform)
+    testset.targets  = [int(t!=normal_class_indx) for t in testset.targets]
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    trainset_msad = MNIST(root=path, train=True, download=True, transform=Transform(bw=True))
+    trainset_msad.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
+    trainset_msad.targets  = [0 for t in trainset.targets]
+    train_loader_msad = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return train_loader, test_loader, train_loader_msad
+
+
+def get_FASHION_MNIST(normal_class_indx, batch_size, path, backbone):
+    transform = transform_color_bw if backbone == 152 else transform_resnet18_bw
+
+    trainset = FashionMNIST(root=path, train=True, download=True, transform=transform)
+    trainset.data = trainset.data[np.array(trainset.targets) == normal_class_indx]
+    trainset.targets  = [0 for t in trainset.targets]
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testset = FashionMNIST(root=path, train=False, download=True, transform=transform)
+    testset.targets  = [int(t!=normal_class_indx) for t in testset.targets]
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    trainset_msad = MNIST(root=path, train=True, download=True, transform=Transform(bw=True))
+    trainset_msad.data = trainset_msad.data[np.array(trainset_msad.targets) == normal_class_indx]
+    trainset_msad.targets  = [0 for t in trainset_msad.targets]
+    train_loader_msad = torch.utils.data.DataLoader(trainset_msad, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return train_loader, test_loader, train_loader_msad
+
+def get_SVHN(normal_class_indx, batch_size, path, backbone):
+    transform = transform_color if backbone == 152 else transform_resnet18
+
+    trainset = SVHN(root=path, split='train', download=True, transform=transform)
+    trainset.data = trainset.data[np.array(trainset.labels) == normal_class_indx]
+    trainset.labels  = [0 for t in trainset.labels]
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    testset = SVHN(root=path, split='test', download=True, transform=transform)
+    testset.labels  = [int(t!=normal_class_indx) for t in testset.labels]
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    trainset_msad = SVHN(root=path, split='train', download=True, transform=Transform())
+    trainset_msad.data = trainset_msad.data[np.array(trainset_msad.labels) == normal_class_indx]
+    trainset_msad.labels  = [0 for t in trainset_msad.labels]
+    train_loader_msad = torch.utils.data.DataLoader(trainset_msad, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return train_loader, test_loader, train_loader_msad
+
+
+class MVTecDataset(Dataset):
+    def __init__(self, root, category, transform=None, target_transform=None, train=True, normal=True):
+        self.transform = transform
+        if train:
+            self.image_files = glob(
+                os.path.join(root, category, "train", "good", "*.png")
+            )
+        else:
+          image_files = glob(os.path.join(root, category, "test", "*", "*.png"))
+          normal_image_files = glob(os.path.join(root, category, "test", "good", "*.png"))
+          anomaly_image_files = list(set(image_files) - set(normal_image_files))
+          self.image_files = image_files
+
+        self.image_files.sort(key=lambda y: y.lower())
+        self.train = train
+
+    def __getitem__(self, index):
+        image_file = self.image_files[index]
+        image = Image.open(image_file)
+        image = image.convert('RGB')
+        if self.transform is not None:
+            image = self.transform(image)
+
+        if os.path.dirname(image_file).endswith("good"):
+            target = 0
+        else:
+            target = 1
+
+        return image, target
+
+    def __len__(self):
+        return len(self.image_files)
+
+
+def get_MVTEC(normal_class_indx, batch_size, path, backbone):
+    normal_class = mvtec_labels[normal_class_indx]
+    transform = transform_color if backbone == 152 else transform_resnet18
+
+    trainset = MVTecDataset(path, normal_class, transform, train=True)
+    train_loader = torch.utils.data.DataLoader(trainset, shuffle=True, batch_size=batch_size)
+
+    testset = MVTecDataset(path, normal_class, transform, train=False)
+    test_loader = torch.utils.data.DataLoader(testset, shuffle=False, batch_size=batch_size)  
+
+    trainset_msad = MVTecDataset(path, normal_class, Transform(), train=True)
+    train_loader_msad = torch.utils.data.DataLoader(trainset_msad, shuffle=True, batch_size=batch_size)
+
+    return train_loader, test_loader, train_loader_msad
