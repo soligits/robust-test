995c13e5bf4f2ac1021522ff481783e1b66a6676
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-04-21 16:22:29 +0430

    add different outlier support

--- PANDA/outlier_exposure.py
+++ PANDA/outlier_exposure.py
@@ -4,22 +4,27 @@ from sklearn.metrics import roc_auc_score
 import torch.optim as optim
 import argparse
 import utils
+import torchattacks
+import torch
+from tqdm import tqdm
+
 
 def train_model(model, train_loader, outliers_loader, test_loader, device, epochs, lr):
     model.eval()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
-    bce = torch.nn.BCELoss()
+    ce = torch.nn.CrossEntropyLoss()
     for epoch in range(epochs):
-        running_loss = run_epoch(model, train_loader, outliers_loader, optimizer, bce, device)
-        print('Epoch: {}, Loss: {}'.format(epoch + 1, running_loss))
-        auc = get_score(model, device, test_loader)
-        print('Epoch: {}, AUROC is: {}'.format(epoch + 1, auc))
+        running_loss = run_epoch(
+            model, train_loader, outliers_loader, optimizer, ce, device
+        )
+        print("Epoch: {}, Loss: {}".format(epoch + 1, running_loss))
+        auc = get_score(model, test_loader, device)
+        print("Epoch: {}, AUROC is: {}".format(epoch + 1, auc))
 
 
-def run_epoch(model, train_loader, outliers_loader, optimizer, bce, device):
+def run_epoch(model, train_loader, outliers_loader, optimizer, ce, device):
     running_loss = 0.0
     for i, (imgs, _) in enumerate(train_loader):
-
         imgs = imgs.to(device)
 
         out_imgs, _ = next(iter(outliers_loader))
@@ -28,16 +33,16 @@ def run_epoch(model, train_loader, outliers_loader, optimizer, bce, device):
 
         optimizer.zero_grad()
 
-        pred, _ = model(imgs)
-        outlier_pred, _ = model(outlier_im)
+        pred = model(imgs)
+        outlier_pred = model(outlier_im)
 
         batch_1 = pred.size()[0]
         batch_2 = outlier_pred.size()[0]
 
-        labels = torch.zeros(size=(batch_1 + batch_2,), device=device)
+        labels = torch.zeros(size=(batch_1 + batch_2,), device=device, dtype=torch.long)
         labels[batch_1:] = torch.ones(size=(batch_2,))
 
-        loss = bce(torch.sigmoid(torch.cat([pred, outlier_pred])), labels)
+        loss = ce(torch.cat([pred, outlier_pred]), labels)
 
         loss.backward()
 
@@ -50,54 +55,127 @@ def run_epoch(model, train_loader, outliers_loader, optimizer, bce, device):
     return running_loss / (i + 1)
 
 
-
-def get_score(model, device, test_loader):
+def get_score(model, test_loader, device):
+    is_train = model.training
     model.eval()
-    anom_labels = []
-    predictions = []
+
+    soft = torch.nn.Softmax(dim=1)
+    anomaly_scores = []
+    preds = []
+    test_labels = []
+
     with torch.no_grad():
-        for imgs, labels in test_loader:
-            imgs, labels = imgs.to(device), labels.numpy()
-            pred, _ = model(imgs)
-            pred = torch.sigmoid(pred)
-            batch_size = imgs.shape[0]
-            for j in range(batch_size):
-                predictions.append(pred[j].detach().cpu().numpy())
-                anom_labels.append(labels[j])
+        with tqdm(test_loader, unit="batch") as tepoch:
+            torch.cuda.empty_cache()
+            for i, (data, target) in enumerate(tepoch):
+                data, target = data.to(device), target.to(device)
+                target = target.type(torch.LongTensor).cuda()
+                output = model(data)
 
-    test_set_predictions = np.array(predictions)
-    test_labels = np.array(anom_labels)
+                predictions = output.argmax(dim=1, keepdim=True).squeeze()
+                preds += predictions.detach().cpu().numpy().tolist()
 
-    auc = roc_auc_score(test_labels, test_set_predictions)
+                probs = soft(output).squeeze()
+                anomaly_scores += probs[:, -1].detach().cpu().numpy().tolist()
+
+                target = target == 1
+
+                test_labels += target.detach().cpu().numpy().tolist()
+
+    auc = roc_auc_score(test_labels, anomaly_scores)
+
+    if is_train:
+        model.train()
+    else:
+        model.eval()
 
     return auc
 
+
+def get_score_adversarial(model, test_loader, test_attack, device):
+    is_train = model.training
+    model.eval()
+
+    soft = torch.nn.Softmax(dim=1)
+    anomaly_scores = []
+    preds = []
+    test_labels = []
+
+    with tqdm(test_loader, unit="batch") as tepoch:
+        torch.cuda.empty_cache()
+        for i, (data, target) in enumerate(tepoch):
+            data, target = data.to(device), target.to(device)
+
+            adv_data = test_attack(data, target)
+            output = model(adv_data)
+
+            predictions = output.argmax(dim=1, keepdim=True).squeeze()
+            preds += predictions.detach().cpu().numpy().tolist()
+
+            probs = soft(output).squeeze()
+            anomaly_scores += probs[:, -1].detach().cpu().numpy().tolist()
+
+            target = target == 1
+
+            test_labels += target.detach().cpu().numpy().tolist()
+
+    auc = roc_auc_score(test_labels, anomaly_scores)
+
+    if is_train:
+        model.train()
+    else:
+        model.eval()
+
+    return auc
+
+
 def main(args):
-    print('Dataset: {}, Label: {}, LR: {}'.format(args.dataset, args.label, args.lr))
+    print("Dataset: {}, Label: {}, LR: {}".format(args.dataset, args.label, args.lr))
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = utils.get_resnet_model(resnet_type=args.resnet_type)
 
     # Change last layer
-    model.fc = torch.nn.Linear(args.latent_dim_size, 1)
+    model.fc = torch.nn.Linear(args.latent_dim_size, 2)
 
     model = model.to(device)
     utils.freeze_parameters(model, train_fc=True)
 
-    train_loader, test_loader = utils.get_loaders(dataset=args.dataset, label_class=args.label, batch_size=args.batch_size)
+    train_loader, test_loader = utils.get_loaders(
+        dataset=args.dataset,
+        label_class=args.label,
+        batch_size=args.batch_size,
+        path="./",
+    )
     outliers_loader = utils.get_outliers_loader(args.batch_size)
 
-    train_model(model, train_loader, outliers_loader, test_loader, device, args.epochs, args.lr)
+    train_model(
+        model, train_loader, outliers_loader, test_loader, device, args.epochs, args.lr
+    )
+
+    print(get_score(model, test_loader=test_loader, device=device))
+    utils.unfreeze_parameters(model)
+    test_attack = torchattacks.PGD(model, steps=10, eps=8 / 255, alpha=2 / 255)
+    print(get_score_adversarial(model, test_loader, test_attack, device))
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--dataset', default='cifar10')
-    parser.add_argument('--epochs', default=50, type=int, metavar='epochs', help='number of epochs')
-    parser.add_argument('--label', default=0, type=int, help='The normal class')
-    parser.add_argument('--lr', type=float, default=1e-1, help='The initial learning rate.')
-    parser.add_argument('--resnet_type', default=152, type=int, help='which resnet to use')
-    parser.add_argument('--latent_dim_size', default=2048, type=int)
-    parser.add_argument('--batch_size', default=32, type=int)
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--dataset", default="cifar10")
+    parser.add_argument(
+        "--outlier", default="tiny", choices=["tiny", "SVHN", "MNIST", "gaussian_noise"]
+    )
+    parser.add_argument(
+        "--epochs", default=50, type=int, metavar="epochs", help="number of epochs"
+    )
+    parser.add_argument("--label", default=0, type=int, help="The normal class")
+    parser.add_argument(
+        "--lr", type=float, default=1e-1, help="The initial learning rate."
+    )
+    parser.add_argument(
+        "--resnet_type", default=152, type=int, help="which resnet to use"
+    )
+    parser.add_argument("--latent_dim_size", default=2048, type=int)
+    parser.add_argument("--batch_size", default=32, type=int)
     args = parser.parse_args()
 
     main(args)
--- PANDA/utils.py
+++ PANDA/utils.py
@@ -6,19 +6,36 @@ import faiss
 import ResNet
 from dataset import get_dataloader
 
-mvtype = ['bottle', 'cable', 'capsule', 'carpet', 'grid', 'hazelnut', 'leather',
-          'metal_nut', 'pill', 'screw', 'tile', 'toothbrush', 'transistor',
-          'wood', 'zipper']
+mvtype = [
+    "bottle",
+    "cable",
+    "capsule",
+    "carpet",
+    "grid",
+    "hazelnut",
+    "leather",
+    "metal_nut",
+    "pill",
+    "screw",
+    "tile",
+    "toothbrush",
+    "transistor",
+    "wood",
+    "zipper",
+]
 
-transform_color = transforms.Compose([transforms.Resize(256),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor()])
+transform_color = transforms.Compose(
+    [transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor()]
+)
 
-transform_gray = transforms.Compose([
-                                 transforms.Resize(256),
-                                 transforms.CenterCrop(224),
-                                 transforms.Grayscale(num_output_channels=3),
-                                 transforms.ToTensor()])    
+transform_gray = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),
+    ]
+)
 
 
 def get_resnet_model(resnet_type=152):
@@ -33,7 +50,7 @@ def get_resnet_model(resnet_type=152):
         return ResNet.wide_resnet50_2(pretrained=True, progress=True)
     elif resnet_type == 101:
         return ResNet.resnet101(pretrained=True, progress=True)
-    else:  #152
+    else:  # 152
         return ResNet.resnet152(pretrained=True, progress=True)
 
 
@@ -41,6 +58,7 @@ def freeze_model(model):
     for param in model.parameters():
         param.requires_grad = False
     return
+
 
 def freeze_parameters(model, train_fc=False):
     for p in model.conv1.parameters():
@@ -55,6 +73,12 @@ def freeze_parameters(model, train_fc=False):
         for p in model.fc.parameters():
             p.requires_grad = False
 
+
+def unfreeze_parameters(model):
+    for p in model.parameters():
+        p.requires_grad = False
+
+
 def knn_score(train_set, test_set, n_neighbours=2):
     """
     Calculates the KNN distance
@@ -64,25 +88,58 @@ def knn_score(train_set, test_set, n_neighbours=2):
     D, _ = index.search(test_set, n_neighbours)
     return np.sum(D, axis=1)
 
-def get_outliers_loader(batch_size):
-    dataset = torchvision.datasets.ImageFolder(root='./data/tiny', transform=transform_color)
-    outlier_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+
+class GaussianNoiseDataset(torch.utils.data.Dataset):
+    def __init__(self, num_samples, img_shape):
+        self.num_samples = num_samples
+        self.img_shape = img_shape
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        noise = torch.randn(self.img_shape)
+        return noise, 1
+
+
+def get_outliers_loader(batch_size, dataset_name="tiny"):
+    if dataset_name == "MNIST":
+        dataset = torchvision.datasets.MNIST(
+            root="./data/MNIST", train=True, transform=transform_gray, download=True
+        )
+    elif dataset_name == "SVHN":
+        dataset = torchvision.datasets.SVHN(
+            root="./data/SVHN", split="train", transform=transform_color, download=True
+        )
+    elif dataset_name == "tiny":
+        dataset = torchvision.datasets.ImageFolder(
+            root="./data/tiny", transform=transform_color
+        )
+    elif dataset_name == "gaussian_noise":
+        dataset = GaussianNoiseDataset(num_samples=5000, img_shape=(3, 224, 224))
+    else:
+        raise ValueError(f"Unsupported dataset_name: {dataset_name}")
+
+    outlier_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=True, num_workers=0
+    )
+
     return outlier_loader
 
+
 def get_loaders(dataset, path, label_class, batch_size):
-    if dataset in ['cifar10', 'cifar100', 'fashion', 'mnist', 'mvtec', 'svhn']:
+    if dataset in ["cifar10", "cifar100", "fashion", "mnist", "mvtec", "svhn"]:
         return get_dataloader(dataset, path, label_class, batch_size)
     else:
-        print('Unsupported Dataset')
+        print("Unsupported Dataset")
         exit()
 
+
 def clip_gradient(optimizer, grad_clip):
-    assert grad_clip>0, 'gradient clip value must be greater than 1'
+    assert grad_clip > 0, "gradient clip value must be greater than 1"
     for group in optimizer.param_groups:
-        for param in group['params']:
+        for param in group["params"]:
             # gradient
             if param.grad is None:
                 continue
             param.grad.data.clamp_(-grad_clip, grad_clip)
-
-
