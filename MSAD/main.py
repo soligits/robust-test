ff0ccc40dbd11ac52b676657d16d923ee2bfbf1f
soligits <m.soltani.252@gmail.com>
2023-08-12 14:49:01 +0330

    test some features

--- MSAD/main.py
+++ MSAD/main.py
@@ -139,6 +139,7 @@ def get_score(model, device, train_loader, test_loader, randomized_smoothing=Fal
     distances = utils.knn_score(train_feature_space, test_feature_space)
     if randomized_smoothing:
         distances = torch.tensor(distances).view(n, -1)
+        print(distances[:, 0])
         distances = distances.mean(0)
         distances = distances.cpu().numpy()
 
